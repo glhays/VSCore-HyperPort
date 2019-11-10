6ffca5e67c01e7ba93e1236f82cc755ea6167b10
Greg Hays <glhays@pacbell.net>
2019-11-09 16:12:12 -0800

    Add Script Tag Data Models
    Adds a domain model for handling api script tag keys for social media and others

---
+++ src/HyperCore.WebUi/Infrastructure/AppSettingsModels/ScriptTags.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyperCore.WebUi.Infrastructure.AppSettingsModels
+{
+    public class ScriptTags
+    {
+        public string GoogleMapsApiKey { get; set; }
+        public string GoogleTagManager { get; set; }
+        public string GoogleAnalytics { get; set; }
+        public string Facebook { get; set; }
+        public string FacebookOpenGraph { get; set; }
+    }
+}
