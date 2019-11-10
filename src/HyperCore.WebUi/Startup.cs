fc7a775bc3fea345366c23d6da2c703098c8cade
Greg Hays <glhays@pacbell.net>
2019-11-09 19:26:00 -0800

    Configure Startup Script Tags for DI
    Configure in the startup  dependency injection

--- src/HyperCore.WebUi/Startup.cs
+++ src/HyperCore.WebUi/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HyperCore.WebUi.Infrastructure.AppSettingsModels;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -20,13 +21,14 @@ namespace HyperCore.WebUi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Retrieve the Script Tag keys from appsettings
+            services.Configure<ScriptTags>(Configuration.GetSection(nameof(ScriptTags)));
+
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,7 +38,7 @@ namespace HyperCore.WebUi
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
